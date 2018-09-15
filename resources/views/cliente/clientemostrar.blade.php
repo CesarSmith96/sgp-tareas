74895f74b2d888e0d94c39335642cabe99da4c2f
Jorge <jorgegarba@gmail.com>
2018-09-15 08:55:36 -0500

    Data Table
    Se personlazina el datatable para ver la lista de clientes.

--- resources/views/cliente/clientemostrar.blade.php
+++ resources/views/cliente/clientemostrar.blade.php
@@ -23,8 +23,111 @@ function setModal(btn){
     request.fail(function(jqXHR, textStatus) {
           alert(textStatus);
     });
+
 }
+
+var DatatableBasic = function() {
+
+
+    //
+    // Setup module components
+    //
+
+    // Basic Datatable examples
+    var _componentDatatableBasic = function() {
+        if (!$().DataTable) {
+            console.warn('Warning - datatables.min.js is not loaded.');
+            return;
+        }
+
+        // Setting datatable defaults
+        $.extend( $.fn.dataTable.defaults, {
+            autoWidth: false,
+            columnDefs: [{ 
+                orderable: false,
+                width: 100,
+                targets: [ 2 ]
+            }],
+            dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
+            language: {
+                search: '<span>Filter:</span> _INPUT_',
+                searchPlaceholder: 'Type to filter...',
+                lengthMenu: '<span>Show:</span> _MENU_',
+                paginate: { 'first': 'First', 'last': 'Last', 'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;' }
+            }
+        });
+
+        // Basic datatable
+        $('.datatable-basic').DataTable();
+
+        // Alternative pagination
+        $('.datatable-pagination').DataTable({
+            pagingType: "simple",
+            language: {
+                paginate: {'next': $('html').attr('dir') == 'rtl' ? 'Next &larr;' : 'Next &rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr; Prev' : '&larr; Prev'}
+            }
+        });
+
+        // Datatable with saving state
+        $('.datatable-save-state').DataTable({
+            stateSave: true
+        });
+
+        // Scrollable datatable
+        var table = $('.datatable-scroll-y').DataTable({
+            autoWidth: true,
+            scrollY: 300
+        });
+
+        // Resize scrollable table when sidebar width changes
+        $('.sidebar-control').on('click', function() {
+            table.columns.adjust().draw();
+        });
+    };
+
+    // Select2 for length menu styling
+    var _componentSelect2 = function() {
+        if (!$().select2) {
+            console.warn('Warning - select2.min.js is not loaded.');
+            return;
+        }
+
+        // Initialize
+        $('.dataTables_length select').select2({
+            minimumResultsForSearch: Infinity,
+            dropdownAutoWidth: true,
+            width: 'auto'
+        });
+    };
+
+
+    //
+    // Return objects assigned to module
+    //
+
+    return {
+        init: function() {
+            _componentDatatableBasic();
+            _componentSelect2();
+        }
+    }
+}();
+
+
+// Initialize module
+// ------------------------------
+
+document.addEventListener('DOMContentLoaded', function() {
+    DatatableBasic.init();
+});
+
+
+
 </script>
+
+<script src="{{asset('global_assets/js/plugins/tables/datatables/datatables.min.js')}}"></script>
+
+
 @endsection
 
 @section('content')
@@ -132,23 +235,19 @@ function setModal(btn){
                             <div class="source-preview-wrapper">
                                 <div class="preview">
                                     <div class="preview-elements">
-                                        <table id="sample-data-table" class="table">
+                                        
+
+                                        <table class="table datatable-basic">
                                             <thead>
                                                 <tr>
-                                                    <th class="secondary-text">
-                                                        <div class="table-header">
-                                                            <span class="column-title">Id</span>
-                                                        </div>
+                                                    <th>
+                                                        Id
                                                     </th>
-                                                    <th class="secondary-text">
-                                                        <div class="table-header">
-                                                            <span class="column-title">Nombre</span>
-                                                        </div>
+                                                    <th>
+                                                        Nombre
                                                     </th>
-                                                    <th class="secondary-text">
-                                                        <div class="table-header">
-                                                            <span class="column-title">Acciones</span>
-                                                        </div>
+                                                    <th>
+                                                        Acciones
                                                     </th>
                                                 </tr>
                                             </thead>
@@ -170,12 +269,7 @@ function setModal(btn){
                                             @endif
                                             </tbody>
                                         </table>
-                                        <script type="text/javascript">
-                                            $('#sample-data-table').DataTable({
-                                            responsive: true
-                                            });
-                                        </script>
-
+                                        
                                     </div>
                                 </div>
                                 
